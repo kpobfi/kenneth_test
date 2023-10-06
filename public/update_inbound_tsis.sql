c9ddbfbfdefe3156c86bc4f8f3f1f331dc36992b
Kenneth Po <kenneth.po@biotechfarms.net>
2023-10-06 10:10:46 +0800

    update 6

--- public/update_inbound_tsis.sql
+++ public/update_inbound_tsis.sql
@@ -1,17 +1,15 @@
-create procedure update_inbound(IN inbound_id integer, IN inbound_weight numeric, IN user_id integer, IN transaction_id integer)
+create procedure update_inbound(IN inbound_id integer, IN inbound_weight numeric)
     language plpgsql
 as
 $$
 BEGIN
     UPDATE inboundweight
-    SET inbound_weight = $2,
-        userid = $3,
-        transactionId = $4
+    SET inbound_weight = $2
     WHERE inboundweight.inboundid = $1;
 
     COMMIT;
 END;
 $$;
 
-alter procedure update_inbound(integer, numeric, integer, integer) owner to postgres;
+alter procedure update_inbound(integer, numeric) owner to postgres;
 
--- public/update_outbound_tsis.sql
+++ public/update_outbound_tsis.sql
@@ -1,17 +1,14 @@
-create procedure update_outbound(IN outbound_id integer, IN outbound_weight numeric, IN user_id integer, IN transaction_id integer)
+create procedure update_outbound(IN outbound_id integer, IN outbound_weight numeric)
     language plpgsql
 as
 $$
 BEGIN
     UPDATE outboundweight
-    SET outbound_weight = $2,
-        userid = $3,
-        transactionid = $4
+    SET outbound_weight = $2
     WHERE outboundweight.outboundid = $1;
-
     COMMIT;
 END;
 $$;
 
-alter procedure update_outbound(integer, numeric, integer, integer) owner to postgres;
+alter procedure update_outbound(integer, numeric) owner to postgres;
 
